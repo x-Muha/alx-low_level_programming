1d27c6e89e0dbfce0dc8f39fade355657864ebf1
muha <muhmdusam4@gmail.com>
2023-02-18 11:52:55 -0800

    task4 2nd attempt

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
-#!/bin/bash
 #include <stdio.h>
 int main(void) {
-	char str[]="Programming is like building a multilingual puzzle";
+	char str[]="\"Programming is like building a multilingual puzzle";
 	puts(str);
 	return 0;
 }
