a16486bbfd798b1ff01e937e53f77c1035e12898
muha <muhmdusam4@gmail.com>
2023-02-18 12:02:33 -0800

    task4 5th attempt

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-/* *
- *  Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
- *   Use the function puts 
- *   You are not allowed to use printf 
- *   Your program should end with the value 0
- * */
+/*
+ * Write a C program that prints exactly
+ * "Programming is like building a multilingual puzzle
+ * Use the function puts
+ * You are not allowed to use printf
+ * Your program should end with the value 0
+ */
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
