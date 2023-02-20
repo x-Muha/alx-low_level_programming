096ac32d6ee826192bed7d4b39974528b182850b
muha <muhmdusam4@gmail.com>
2023-02-20 15:06:41 -0800

    task9 2nd attempt

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,12 +7,12 @@ int main(void)
 {
 	int i;
 
+	putchar('0');
 	for (i = 0; i <= 9; i++)
 	{
-		putchar('0' + i);
 		putchar(',');
 		putchar(' ');
+		putchar('1' + i);
 	}
-	putchar('\n');
 	return (0);
 }
