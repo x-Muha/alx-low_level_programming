e9571e21aa65eed7d1a471e1ab7485975977dd3e
muha <muhmdusam4@gmail.com>
2023-02-20 15:09:53 -0800

    task9 3rd attempt[added if statement for the last event]

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,12 +7,14 @@ int main(void)
 {
 	int i;
 
-	putchar('0');
 	for (i = 0; i <= 9; i++)
 	{
+		putchar('0' + i);
+		if (i == 9)
+			break;
 		putchar(',');
 		putchar(' ');
-		putchar('1' + i);
 	}
+	putchar('\n');
 	return (0);
 }
