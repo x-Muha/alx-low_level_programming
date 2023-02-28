9436317d1767cabb1e8513833f86fd8fbf56e275
muha <muhmdusam4@gmail.com>
2023-02-28 13:19:46 -0800

    task1 1st attempt

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int - function
+ * @a: 1st
+ * @b: 2nd
+ * Return: 0
+ */
+void swap_int(int *a, int *b);
+{
+	int temp;
+
+	temp = a;
+	a = b;
+	b = temp;
+	return (0);
+}
