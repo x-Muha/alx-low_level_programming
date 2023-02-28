191d1d37390f600878d57f9cfdece1480a6958a7
muha <muhmdusam4@gmail.com>
2023-02-28 14:09:47 -0800

    tsk7 1st attempt

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts_half - fun
+ * @str: string
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len;
+	int i;
+
+	len = _strlen(*str);
+	for (i = len / 2; i < len / 2; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
