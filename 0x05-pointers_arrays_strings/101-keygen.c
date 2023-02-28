238d3595f2ee65f1db80ef0b03ae7ab569474f95
muha <muhmdusam4@gmail.com>
2023-02-28 14:29:43 -0800

    many

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -18,8 +18,8 @@ int main(void)
 	for (i = 0; i < 100; i++)
 	{
 		pw[i] = rand() % 78;
-		sum += (pass[i] + '0');
-		putchar(pass[i] + '0');
+		sum += (pw[i] + '0');
+		putchar(pw[i] + '0');
 		if ((2772 - sum) - '0' < 78)
 		{
 			n = 2772 - sum - '0';
