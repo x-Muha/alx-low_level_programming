ea84499cfc7a65ad8f3d00ef3915703baeaa7cc4
muha <muhmdusam4@gmail.com>
2023-03-07 11:29:28 -0800

    tsk2

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memcpy - func
+ * @dest: dest
+ * @src: src
+ * @n: unsigned int
+ * Return: pointer
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int r = 0;
+	int i = n;
+
+	for (; r < i; r++)
+	{
+		dest[r] = src[r];
+		n--;
+	}
+	return (dest);
+}
