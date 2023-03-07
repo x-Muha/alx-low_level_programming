20018dce7407e744375b891e672bed57cd91dd21
muha <muhmdusam4@gmail.com>
2023-03-07 09:38:34 -0800

    tsk0 1st attempt

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _memset - func
+ * @s: char to edit on
+ * @b: char to put
+ * @n: first bits
+ * Return: pointer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	s[i] = b;
+	return (s);
+}
