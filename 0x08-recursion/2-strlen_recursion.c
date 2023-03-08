40a7577e7fd0b7bffa47a6abb2edd0ff5c4b794e
muha <muhmdusam4@gmail.com>
2023-03-08 13:45:20 -0800

    tsk2 1st attempt

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - func
+ * @s: char
+ * Return: int
+ */
+int _strlen_recursion(char *s)
+{
+	int len;
+
+	if (*s)
+	{
+		len++;
+		len = len + _strlen_recursion(s + 1);
+	}
+	return (int);
+}
