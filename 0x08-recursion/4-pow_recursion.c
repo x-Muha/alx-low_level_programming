730e9481815fc7721cf7a6b14c5b9f3863af4808
muha <muhmdusam4@gmail.com>
2023-03-08 14:07:13 -0800

    tsk4 2nd attempt

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -5,11 +5,11 @@
  * @y: power
  * Return: int
  */
-int _pow_recursion(int x,int y)
+int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
 	if (y == 0)
 		return (1);
-	return (x * _pow_recursion(x, y-1));
+	return (x * _pow_recursion(x, y - 1));
 }
